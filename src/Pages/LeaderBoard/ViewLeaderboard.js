9b70ade8ceb2e70cfa0a500945952c7b367342e4
Dharmpal123 <dharmpal.singh@lmsin.com>
2023-02-06 12:04:28 +0530

    leader board listing design

--- src/Pages/LeaderBoard/ViewLeaderboard.js
+++ src/Pages/LeaderBoard/ViewLeaderboard.js
@@ -3,13 +3,16 @@ import { Container, Row, Col, Card, NavLink } from "react-bootstrap";
 import Image from "../../Components/image/Image";
 import Text from "../../Components/text/Text";
 import Heading from "../../Components/heading/Heading";
+import Buttons from "../../Components/Buttons/Button";
 import UserPostOne from '../../assets/images/user-post.svg';
 import UserPostTwo from '../../assets/images/user-post-two.svg';
 import UserPostThree from '../../assets/images/user-post-three.svg';
-import SearchComponent from "../../Components/Search/Search"; 
+import SearchComponent from "../../Components/Search/Search";
+import Input from "../../Components/Input/Input";
+import SelectFloat from "../../Components/Input/SelectFloat";
 
 // images and icons 
-import BackArrow from '../../assets/images/svg/backArrow.svg'; 
+import BackArrow from '../../assets/images/svg/backArrow.svg';
 
 const ViewleaderBoard = (props) => {
     return (
@@ -20,18 +23,61 @@ const ViewleaderBoard = (props) => {
                         <Image src={BackArrow} className="img-fluid" />
                         <span className='text-col2 fs-25 ms-2 fw500'>Back</span></NavLink>
                 </div>
+                <Row className="d-flex align-items-center justify-content-between mb-4">
+                    <Col md={3}>
+                        <Heading
+                            color='text-col1'
+                            fontWeight='fw600'
+                            className='fs-25 w-100 mb-0'
+                            typeofHeading='h1'
+                            text='Leaderboard'
+                        />
+                    </Col>
+                    <Col md={9}>
+                        <div className="text-start mt-3 mt-sm-0 text-sm-end">
+                            <Buttons
+                                text="View Proposed Leaderboards"
+                                variant="primary"
+                                className="primary pad1 fs-14 me-3 mb-3 mb-sm-0 fw600"
+                            />
+                            <Buttons
+                                text="Create Leaderboard"
+                                variant="primary"
+                                className="btn-primary btn-reset pad1 fs-14 fw600"
+                            />
+                        </div>
+                    </Col>
+                </Row>
                 <Card>
                     <Card.Body>
-                        <div className="mb-3 mb-md-0">
-                            <Text text="My Friends (07)"
-                                color="text-col1"
-                                className="fs-20 fw600 mb-2" />
-                            <div className="d-flex checkbox-types mb-3">
-                            </div>
-                        </div>
-                        <Row>
-                            <Col md={6}>
+                        <Row className="mt-3">
+                            <Col md={4}>
                                 <SearchComponent text="text" name="search" placeholder="Find People" />
+                            </Col>
+                            <Col md={7} className="ms-auto">
+                                <div className="view-board-input">
+                                    <Row>
+                                        <Col md={4}>
+                                            <Input placeholder="Platform" />
+                                        </Col>
+                                        <Col md={4}>
+                                            <Input placeholder="Game" />
+                                        </Col>
+                                        <Col md={4}>
+                                            <Input placeholder="Player" />
+                                        </Col>
+                                    </Row>
+                                </div>
+                            </Col>
+                        </Row>
+                        <div className="border-bottom-lightgray mt-0 mb-4"></div>
+                        <Row>
+                            <Col md={6}></Col> 
+                            <Col md={3}>
+                                <SelectFloat defaultOption="Sort By Record Count" />
+                            </Col>
+                            <Col md={3}>
+                                <SelectFloat defaultOption="Order By DESC" />  
                             </Col>
                         </Row>
                         <div className="user-list">
@@ -63,9 +109,6 @@ const ViewleaderBoard = (props) => {
                                         <div className="text-start text-sm-end mt-3 mt-sm-0">
                                             <span className="text-col4 fs-16 remove fw600">Remove</span>
                                             <span className="ms-3 status-default pending">Pending</span>
-                                            {/* <span className="ms-3 status-default accept">Accept</span> 
-                    <span className="ms-3 status-default follow">Follow</span> 
-                    <span className="ms-3 status-default unfollow">Unfollow</span>    */}
                                         </div>
                                     </Col>
                                 </Row>
